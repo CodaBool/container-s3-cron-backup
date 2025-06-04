310d7199e3a56c5516647104b2b55135f9762d3e
Coda Bool <CodaBool@pm.me>
2025-06-03 23:20:30 -0400

    make more defaults

--- dobackup.sh
+++ dobackup.sh
@@ -5,17 +5,16 @@ set -e
 source /home/backup/.env
 
 # default storage class to standard if not provided
-S3_STORAGE_CLASS=${S3_STORAGE_CLASS:-STANDARD}
+S3_STORAGE_CLASS=${S3_STORAGE_CLASS:-GLACIER}
+NAME=${BACKUP_NAME:-backup}
 
 # generate file name for tar
-FILE_NAME=/tmp/${BACKUP_NAME}-$(date "+%Y-%m-%d_%H-%M-%S").tar.gz
+FILE_NAME=/tmp/${NAME}-$(date "+%Y-%m-%d_%H-%M-%S").tar.gz
 
 # Check if TARGET variable is set
 if [ -z "${TARGET}" ]; then
-    echo "TARGET env var is not set so we use the default value (/data)"
-    TARGET=/data
-else
-    echo "TARGET env var is set"
+  echo "TARGET env var is not set so we use the default value (/data)"
+  TARGET=/data
 fi
 
 if [ -z "${S3_ENDPOINT}" ]; then
@@ -24,7 +23,7 @@ else
   AWS_ARGS="--endpoint-url ${S3_ENDPOINT}"
 fi
 
-echo "archiving folders: ${TARGET}\n"
+echo -e "archiving folders: ${TARGET}\n"
 tar -zcf "${FILE_NAME}" ${TARGET} \
   --checkpoint=800000 \
   --checkpoint-action="echo=%T"
